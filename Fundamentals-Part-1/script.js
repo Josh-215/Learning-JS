705578ca649b629ff035d19b9e7899945ab126d2
Josh-215 <urquhartjoshua1@gmail.com>
2021-11-19 15:20:04 -0800

    Values-and-variables

--- Fundamentals-Part-1/script.js
+++ Fundamentals-Part-1/script.js
@@ -1,3 +1,23 @@
 let js = "amazing";
-if (js === "amazing") alert("JavasScript is fun");
 console.log(40 + 8 + 23 - 10);
+console.log("josh");
+// when ever i have multipe word for a variable use
+// camelCase: lowercase for the first word and uppercase for the rest.
+/*
+Exp
+let first = "Josh";
+let firstNameParson = "Josh";
+*/
+// This is OK
+let jonas_matilda = "JM";
+console.log(jonas_matilda);
+//but this is standard
+let firstName = "Josh";
+console.log(firstName);
+
+//its best to have a more descriptive variable
+let myFirstJob = "walmart";
+let myCurrentJob = "programmer";
+// then this
+let job1 = "walmart";
+let job2 = "programmer";
