51e2c1336dbeedfa84cad94ed5ebf003c767c3f8
Josh-215 <urquhartjoshua1@gmail.com>
2021-11-19 17:41:21 -0800

    Data Types

--- Fundamentals-Part-1/assignment.js
+++ Fundamentals-Part-1/assignment.js
@@ -1,6 +1,13 @@
-let conuntry = "The US";
+/*
+1. Declarevariablescalled'country','continent'and'population'and assign their values according to your own country (population in millions)
+2. Log their values to the console 
+*/
+
+let country = "The US";
 console.log(conuntry);
 let continent = "North America";
 console.log(continent);
 let population = "333,680,703";
 console.log(population);
+
+let isIsland = "country";
--- Fundamentals-Part-1/script.js
+++ Fundamentals-Part-1/script.js
@@ -1,13 +1,15 @@
+//values and variables
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 console.log("josh");
 // when ever i have multipe word for a variable use
 // camelCase: lowercase for the first word and uppercase for the rest.
-/*
+
 Exp
 let first = "Josh";
 let firstNameParson = "Josh";
-*/
+
 // This is OK
 let jonas_matilda = "JM";
 console.log(jonas_matilda);
@@ -21,3 +23,26 @@ let myCurrentJob = "programmer";
 // then this
 let job1 = "walmart";
 let job2 = "programmer";
+*/
+
+//data types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof false); //is a boolean
+console.log(typeof javascriptIsFun); //is a boolean
+console.log(typeof 32); // is  a number
+console.log(typeof "hi"); //is a string
+
+/*
+javaceiptIsFUn was a boolean but now it a 
+string because we declare a 'string' and 
+not a true or false.
+*/
+javascriptIsFun = "yes";
+console.log(typeof javascriptIsFun); //string
+
+let year;
+console.log(typeof year); //undfined
+year = 1998;
+console.log(typeof year); // number
