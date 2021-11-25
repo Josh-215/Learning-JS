948723d23c44c5424e79545dbf8f75752f85aa09
Josh-215 <urquhartjoshua1@gmail.com>
2021-11-25 00:22:49 -0800

    Arrow Functions

--- Fundamentals-Part-2/script.js
+++ Fundamentals-Part-2/script.js
@@ -16,10 +16,9 @@ console.log(applejuice);
 
 const applejuices = fruitProcessor(6, 9);
 console.log(applejuices);
-
 const num = Number("23");
+
 ///////
-*/
 ////Function Declarations vs. Expressions
 //this a Function Declarations
 //we can call a Declarations before evan defining them
@@ -39,3 +38,21 @@ const calcAge2 = function (birthYear) {
 };
 const age2 = calcAge2(1991);
 console.log(age1, age2);
+////////////
+*/
+/////
+///Arrow Functions
+
+// const calcAge3 = (birthYear) => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+const yearsOfRetirement = (birthyear, firstName) => {
+  const age = 2037 - birthyear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement}`;
+};
+
+console.log(yearsOfRetirement(1991, "jay"));
+console.log(yearsOfRetirement(1980, "bob"));
