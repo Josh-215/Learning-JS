5c6dfc8945f50fcc7b985bff8c6eb5d7d645e90d
Josh-215 <urquhartjoshua1@gmail.com>
2021-12-04 16:15:08 -0800

    Selecting and Manipulating Elements

--- PROJECT #1: Guess My Number!/script.js
+++ PROJECT #1: Guess My Number!/script.js
@@ -1,1 +1,12 @@
-'use strict';
+"use strict";
+//document.querySelector(".class name"); gets the element with a class name
+console.log(document.querySelector(".message").textContent);
+//.textContent log the class content
+document.querySelector(".message").textContent = "correct Number";
+// you can change elements contents here too
+console.log(document.querySelector(".message").textContent);
+
+console.log(document.querySelector(".score").textContent);
+console.log(document.querySelector(".number").textContent);
+//for the element 'input'you ues .value instead of .textContent to get the value
+console.log(document.querySelector(".guess").value);
