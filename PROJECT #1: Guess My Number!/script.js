ccfa547ed00c033350299376f2f5a2b280dddbec
Josh-215 <urquhartjoshua1@gmail.com>
2021-12-04 18:00:33 -0800

    Handling Click Events

--- PROJECT #1: Guess My Number!/script.js
+++ PROJECT #1: Guess My Number!/script.js
@@ -1,4 +1,5 @@
 "use strict";
+/*
 //document.querySelector(".class name"); gets the element with a class name
 console.log(document.querySelector(".message").textContent);
 //.textContent log the class content
@@ -10,3 +11,13 @@ console.log(document.querySelector(".score").textContent);
 console.log(document.querySelector(".number").textContent);
 //for the element 'input'you ues .value instead of .textContent to get the value
 console.log(document.querySelector(".guess").value);
+*/
+/////
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = document.querySelector(".guess").value;
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number";
+  }
+});
