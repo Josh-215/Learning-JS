7e942b3ed223190e02b3257d165f8854d385d184
Josh-215 <urquhartjoshua1@gmail.com>
2021-12-10 19:53:14 -0800

    Coding Challenge #1

--- PROJECT #1: Guess My Number!/script.js
+++ PROJECT #1: Guess My Number!/script.js
@@ -14,10 +14,19 @@ console.log(document.querySelector(".guess").value);
 */
 /////
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+document.querySelector(".again").addEventListener("click", function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector(".guess").value = "";
+  document.querySelector(".score").textContent = "20";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
 
-document.querySelector(".number").textContent = secretNumber;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
@@ -27,8 +36,9 @@ document.querySelector(".check").addEventListener("click", function () {
     //when you win
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "correct Number";
-    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").textContent = secretNumber;
 
+    document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
 
     // when you guess is to high
