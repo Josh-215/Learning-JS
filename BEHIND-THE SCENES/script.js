8bebb136d2b01497dcba7abffec0ef5be115ea28
Josh-215 <urquhartjoshua1@gmail.com>
2021-12-17 16:04:34 -0800

    Scoping in Practice

--- BEHIND-THE SCENES/script.js
+++ BEHIND-THE SCENES/script.js
@@ -1,14 +1,32 @@
 "use strict";
 function calcAge(birthYear) {
   const age = 2021 - birthYear;
+
   function printAge() {
     const output = `${fristName} you are ${age}, born is ${birthYear}`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const fristName = "jey";
+
+      //resetting outer scopes
+      output = "new output!";
+
+      var millenial = true;
+      const str = `oh, and you're a millenial, ${fristName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str)
+    console.log(millenial);
+    // console.log(add(2, 4));
   }
   printAge();
-
   return age;
 }
 
 const fristName = "josh";
-calcAge(1998);
+calcAge(1991);
