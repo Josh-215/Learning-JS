12f0b1cff2d1c3371f16d3ad5b40d64770e6dc05
Josh-215 <urquhartjoshua1@gmail.com>
2021-12-18 18:37:56 -0800

    coping in Practice

--- BEHIND-THE SCENES/script.js
+++ BEHIND-THE SCENES/script.js
@@ -1,32 +1,37 @@
 "use strict";
+
 function calcAge(birthYear) {
-  const age = 2021 - birthYear;
+  const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `${fristName} you are ${age}, born is ${birthYear}`;
+    let output = `${fristName} you are ${age}, born in ${birthYear} `;
     console.log(output);
 
     if (birthYear >= 1981 && birthYear <= 1996) {
-      const fristName = "jey";
-
-      //resetting outer scopes
-      output = "new output!";
-
+      //crateing new variable with same name as outer scope
+      const fristName = "jay";
       var millenial = true;
-      const str = `oh, and you're a millenial, ${fristName}`;
+      //reasssinging outer scopes variable
+      output = "new output";
+
+      const str = `oh, and your a millenial, ${fristName}`;
       console.log(str);
 
       function add(a, b) {
         return a + b;
       }
     }
-    // console.log(str)
-    console.log(millenial);
-    // console.log(add(2, 4));
+    console.log(millenial); // var is a function scope so it s ignore the block
+    // add(1, 2); Errer: its out of its scope
+    console.log(output);
   }
   printAge();
+
   return age;
 }
 
+//global scope
+//console.log(age) = Error: cant be called the variable was crated in a function scope
 const fristName = "josh";
 calcAge(1991);
+///////
