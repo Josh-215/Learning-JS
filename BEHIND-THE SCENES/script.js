a573f329ba5ab8cce6e21a66dd7f25b198a7b04b
Josh-215 <urquhartjoshua1@gmail.com>
2021-12-19 13:22:20 -0800

    The this Keyword in Practice

--- BEHIND-THE SCENES/script.js
+++ BEHIND-THE SCENES/script.js
@@ -1,37 +1,72 @@
 "use strict";
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    let output = `${fristName} you are ${age}, born in ${birthYear} `;
-    console.log(output);
+//   function printAge() {
+//     let output = `${fristName} you are ${age}, born in ${birthYear} `;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      //crateing new variable with same name as outer scope
-      const fristName = "jay";
-      var millenial = true;
-      //reasssinging outer scopes variable
-      output = "new output";
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       //crateing new variable with same name as outer scope
+//       const fristName = "jay";
+//       var millenial = true;
+//       //reasssinging outer scopes variable
+//       output = "new output";
 
-      const str = `oh, and your a millenial, ${fristName}`;
-      console.log(str);
+//       const str = `oh, and your a millenial, ${fristName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
-    console.log(millenial); // var is a function scope so it s ignore the block
-    // add(1, 2); Errer: its out of its scope
-    console.log(output);
-  }
-  printAge();
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     console.log(millenial); // var is a function scope so it s ignore the block
+//     // add(1, 2); Errer: its out of its scope
+//     console.log(output);
+//   }
+//   printAge();
 
-  return age;
-}
+//   return age;
+// }
 
-//global scope
-//console.log(age) = Error: cant be called the variable was crated in a function scope
-const fristName = "josh";
-calcAge(1991);
-///////
+// //global scope
+// //console.log(age) = Error: cant be called the variable was crated in a function scope
+// const fristName = "josh";
+// calcAge(1991);
+// ///////
+
+// console.log(josh.calcAge());
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2021 - birthYear);
+  console.log(this);
+};
+calcAge(1998);
+
+// the THIS dose not work in an arrow function
+const calcAgeArrow = (birthYear) => {
+  console.log(2027 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1998);
+
+const josh = {
+  year: 1998,
+  calcAge: function () {
+    console.log(this);
+    console.log(2021 - this.year);
+  },
+};
+josh.calcAge();
+
+const jay = {
+  year: 1997,
+};
+
+jay.calcAge = josh.calcAge;
+jay.calcAge();
+
+const f = josh.calcAge;
+f();
